a2a5a9f3640f36557b2a776cb23a4cf9b426659f
Michael Mahoney <michael.mahoney@alphaomega.com.co>
2024-01-07 15:11:10 -0700

    building out some of the core tests. Server ping test and the like.

--- aohttp/tests/http_server_tests.rs
+++ aohttp/tests/http_server_tests.rs
@@ -1,9 +1,39 @@
+use core::panic;
+
 use aohttp;
+use aohttp::http::*;
+use aohttp::router::*;
+
+fn test_constant_setup()
+{
+    let auth: HttpAuth = HttpAuth::Basic(String::from("this is a thing"));
+    const uri: Uri = Uri {
+    path: String::from("test_path"), 
+    query: None, 
+    fragment: None 
+};
+const handler: Option<fn(&Route) -> Result<HttpResponse, Error>> = None;
+const test_route: Route = Route{
+    auth: auth,
+    uri: uri,
+    handler: handler
+};
+}
+
+
+
 
 #[test]
 fn server_ping_test()
 {
-
+    const routes: Vec<&Route> = vec![&test_route];
+    let result = aohttp::run_http_server("127.0.0.1", &routes);
+    match result 
+    {
+        Ok(_) => (),
+        Err(_) => panic!{"It didn't work."}
+    }
+    
 }
 
 #[test]
